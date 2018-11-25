f3d8834347077003e0a7654be880302425b5038e
tsmilgius <tsmilgius@gmail.com>
2018-11-25 14:31:23 +0200

    removed keys

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    googleClientID: '872177646094-ova5t1qsb5s036hfbissfqop76c58ucm.apps.googleusercontent.com', 
-    googleClientSecret: 'Vpgq9VHLrTqI9wQk4tPRtQgf'
-}
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
+
 require('./services/passport');
 
 const app =  express();
