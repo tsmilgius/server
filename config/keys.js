b126e8b4f12f423ea468a0065b43261655ba206a
tsmilgius <tsmilgius@gmail.com>
2018-11-25 19:09:43 +0200

    env variables

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: '872177646094-ova5t1qsb5s036hfbissfqop76c58ucm.apps.googleusercontent.com', 
-    googleClientSecret: 'Vpgq9VHLrTqI9wQk4tPRtQgf', 
-    mongoURI: 'mongodb://user:Labasrytas1982@ds115854.mlab.com:15854/emaily-dev123', 
-    cookieKey: 'teyquwetqwuyteuywqteghjasddhsabnvfvaorfasbkuol'
+if (process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
 }
