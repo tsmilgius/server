28f1ee56a7f8f7e06197c7b5cb3a9356f6f02b0c
tsmilgius <tsmilgius@gmail.com>
2018-11-25 20:16:14 +0200

    dev variables

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '872177646094-ova5t1qsb5s036hfbissfqop76c58ucm.apps.googleusercontent.com', 
+    googleClientSecret: 'Vpgq9VHLrTqI9wQk4tPRtQgf', 
+    mongoURI: 'mongodb://user:Labasrytas1982@ds115854.mlab.com:15854/emaily-dev123', 
+    cookieKey: 'teyquwetqwuyteuywqteghjasddhsabnvfvaorfasbkuol'
+}
